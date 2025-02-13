b6a3144c35708fb3a76dc3280e87f356d4ebe1ec
Brendan J Bond <brendanbond@gmail.com>
2025-02-13 16:19:11 -0600

    WIP: move email rendering to core processors

--- src/core/RootShim.ts
+++ src/core/RootShim.ts
@@ -2,9 +2,9 @@ import { InstanceShim } from './InstanceShim';
 import * as FormioCore from '@formio/core';
 
 export class RootShim {
-    public instanceMap: any;
-    public data: any;
-    public components: any[];
+  public instanceMap: any;
+  public data: any;
+  public components: any[];
 
     private _form: any;
     private _submission: any;
@@ -87,22 +87,66 @@ export class RootShim {
                 }
             }
         }
-        return this.instanceMap[path];
+        const { path, fullPath, fullLocalPath, dataPath, localDataPath } = paths;
+        const instance = new InstanceShim(
+          component,
+          this,
+          submission.data,
+          dataPath ?? path ?? component.key,
+          index,
+        );
+        this.components.push(instance);
+        if (path && !this.instanceMap[path]) {
+          this.instanceMap[path] = instance;
+        }
+        if (fullPath && !this.instanceMap[fullPath]) {
+          this.instanceMap[fullPath] = instance;
+        }
+        if (fullLocalPath && !this.instanceMap[fullLocalPath]) {
+          this.instanceMap[fullLocalPath] = instance;
+        }
+        if (dataPath && !this.instanceMap[dataPath]) {
+          this.instanceMap[dataPath] = instance;
+        }
+        if (localDataPath && !this.instanceMap[localDataPath]) {
+          this.instanceMap[localDataPath] = instance;
+        }
+      },
+      true,
+    );
+  }
+
+  getComponent(pathArg: string | string[]) {
+    const path = FormioCore.Utils.getStringFromComponentPath(pathArg);
+    // If we don't have an exact path match, compare the final pathname segment with the path argument for each component
+    // i.e. getComponent('foo') should return a component at the path 'bar.foo' if it exists
+    if (!this.instanceMap[path]) {
+      for (const key in this.instanceMap) {
+        const match = key.match(new RegExp(`\\.${path}$`));
+        const lastPathSegment = match ? match[0].slice(1) : '';
+        if (lastPathSegment === path) {
+          // set a cache for future `getComponent` calls in this lifecycle
+          this.instanceMap[path] = this.instanceMap[key];
+          break;
+        }
+      }
     }
+    return this.instanceMap[path];
+  }
 
-    get submission() {
-        return this._submission;
-    }
+  get submission() {
+    return this._submission;
+  }
 
-    set submission(data: any) {}
+  set submission(data: any) {}
 
-    get form() {
-        return this._form;
-    }
+  get form() {
+    return this._form;
+  }
 
-    set form(form: any) {}
+  set form(form: any) {}
 
-    get root() {
-        return null;
-    }
+  get root() {
+    return null;
+  }
 }
