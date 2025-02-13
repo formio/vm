b6f9f06d07895091164d990f8b6682d8852818f2
Brendan J Bond <brendanbond@gmail.com>
2025-02-13 16:19:11 -0600

    WIP: move email rendering to core processors

--- src/core/renderEmail.ts
+++ src/core/renderEmail.ts
@@ -1,75 +1,121 @@
 import _ from 'lodash';
-import { Formio, Form } from '@formio/js';
+import {
+  ProcessorContext,
+  ProcessorFn,
+  ProcessorFnSync,
+  ProcessorInfo,
+  ProcessorScope,
+  Utils,
+  processSync,
+} from '@formio/core';
 import { evaluate } from '..';
 
 import macros from './deps/nunjucks-macros';
 
 export type RenderEmailOptions = {
-    render: any;
-    context: any;
-    timeout?: number;
+  render: any;
+  context: any;
+  timeout?: number;
+};
+
+const renderEmailProcessorSync: ProcessorFnSync<ProcessorScope> = (context) => {
+  const { component, paths, parent, scope, data } = context;
+  if (!(scope as any).renderEmail) {
+    (scope as any).renderEmail = '';
+  }
+  const scopeRef = scope as any;
+  if (
+    scopeRef.conditionals.find(
+      (cond: any) => cond.path === paths?.fullPath && cond.conditionallyHidden,
+    )
+  ) {
+    return;
+  }
+  let result = '';
+
+  switch (Utils.getModelType(component)) {
+    case 'dataObject':
+    case 'nestedArray':
+    case 'nestedDataArray': {
+      result += `
+          <table border="1" style="width:100%">
+            <tbody>
+              <child />
+            </tbody>
+          </table>
+        `;
+      break;
+    }
+    case 'number':
+    case 'boolean':
+    case 'string': {
+      result += `<tr>
+        <th style="padding: 5px 10px;">${component.label || component.key}</th>
+        <td style="width:100%;padding:5px 10px;">${_.get(
+          data,
+          paths?.fullPath ?? component.key,
+        )}</td>
+      </tr>`;
+      break;
+    }
+    default:
+      break;
+  }
+
+  if (!parent) {
+    (scope as any).renderEmail += result;
+  } else {
+    (scope as any).renderEmail = (scope as any).renderEmail.replace('<child />', result);
+  }
+};
+
+const renderEmailProcessor: ProcessorFn<ProcessorScope> = async (context) => {
+  return renderEmailProcessorSync(context);
+};
+
+const renderEmailProcessorInfo: ProcessorInfo<ProcessorContext<ProcessorScope>, void> = {
+  name: 'renderEmail',
+  process: renderEmailProcessor,
+  processSync: renderEmailProcessorSync,
+  shouldProcess: () => true,
 };
 
 export async function renderEmail({
-    render,
-    context = {},
-    timeout = 500,
+  render,
+  context = {},
+  timeout = 500,
 }: RenderEmailOptions): Promise<string> {
-    if (context._private) {
-        delete context._private;
-    }
-    context.macros = macros;
+  if (context._private) {
+    delete context._private;
+  }
+  context.macros = macros;
 
-    const renderMethod = getRenderMethod(render);
+  const renderMethod = getRenderMethod(render);
 
-    const data: any = {
-        input: omitUndefined(render),
-        context,
-        submissionTableHtml: null,
-    };
+  const data: any = {
+    input: omitUndefined(render),
+    context,
+    submissionTableHtml: null,
+  };
 
-    if (renderMethod === 'dynamic') {
-        try {
-            const premium = await import(
-                // @ts-expect-error Premium is included in the server build.
-                '@formio/premium/dist/premium-server.min.js'
-            );
-            Formio.use(premium);
-        } catch {
-            // eslint-disable-next-line no-empty
-        }
-        const form = await new Form(context.form, {
-            server: true,
-            noeval: true,
-            noDefaults: true,
-            submissionTimezone: context?.metadata?.timezone,
-        }).ready;
-
-        form.setValue(
-            { data: context.data, metadata: context.metadata },
-            { sanitize: true },
-        );
-
-        // this is a hack to ensure the form is fully rendered before we get the form html
-        // ideally, we'd have a promise from the renderer that we can await to ensure the form is fully rendered
-        await new Promise((resolve) => {
-            setTimeout(resolve, 1000);
-        });
-
-        const submissionTableHtml = form.getView(context.data, {
-            email: true,
-        });
-
-        data.submissionTableHtml = submissionTableHtml;
-    }
-
-    const res = await evaluate({
-        deps: ['lodash', 'moment', 'core', 'nunjucks'],
-        data: data,
-        code: getScript(render),
-        timeout,
+  if (renderMethod === 'dynamic') {
+    const result = processSync({
+      ...context,
+      components: context.form.components,
+      processors: [renderEmailProcessorInfo],
     });
-    return res as string;
+    data.submissionTableHtml = `<table border="1" style="width:100%"><tbody>${
+      (result as any).renderEmail
+    }</tbody></table>`;
+  }
+
+  const res = await evaluate({
+    deps: ['lodash', 'moment', 'core', 'nunjucks'],
+    data: data,
+    code: getScript(render),
+    timeout,
+  });
+  return res as string;
 }
 
 export function getScript(data: any) {
@@ -91,7 +137,7 @@ export function getScript(data: any) {
       environment.params = context;
       output = unescape(environment.renderString(sanitize(input), context));
     `;
-    }
+  }
 
     // Script to render an object of properties.
     return `
@@ -112,13 +158,13 @@ export function getScript(data: any) {
 }
 
 function getRenderMethod(render: any) {
-    let renderMethod = 'static';
-    if (process.env.RENDER_METHOD) {
-        renderMethod = process.env.RENDER_METHOD;
-    } else if (render && render.renderingMethod) {
-        renderMethod = render.renderingMethod;
-    }
-    return renderMethod;
+  let renderMethod = 'static';
+  if (process.env.RENDER_METHOD) {
+    renderMethod = process.env.RENDER_METHOD;
+  } else if (render && render.renderingMethod) {
+    renderMethod = render.renderingMethod;
+  }
+  return renderMethod;
 }
 
 const omitUndefined = (obj: any) => _.omitBy(obj, _.isUndefined);
